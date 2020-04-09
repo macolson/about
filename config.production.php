aa0fb074d0986ab1f79d0273894aafc1a8bb14cd
oliver kaspar <macolson@gmail.com>
2020-04-09 11:21:55 +0200

    base url adoptions

--- config.production.php
+++ config.production.php
@@ -3,6 +3,4 @@
 return [
     'production' => true,
     'baseUrl' => 'https://macolson.github.io/macolson.gihub.io/',
-    'title' => 'Oliver Kaspar - Full Stack Developer',
-    'description' => 'Here I am.',
 ];
