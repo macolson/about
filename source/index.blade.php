9d14236cec271c4cb933a846aa1a9a1f7aadc343
oliverkaspar <oliver.kaspar@mossglocal.com>
2025-11-03 13:33:17 +0100

    Add layout structure and sections for index page

---
+++ source/index.blade.php
@@ -0,0 +1,27 @@
+@extends('_layouts.master')
+
+@section('body')
+    <div class="max-w-full lg:max-w-7xl w-full shadow-lg m-auto bg-white flex flex-col lg:flex-row">
+
+        @include('_partials.en.sidebar')
+
+        <div class="w-full lg:w-3/5 p-5 lg:p-10">
+            @include('_partials.en.about')
+            @include('_partials.en.experience')
+            @include('_partials.en.education')
+        </div>
+    </div>
+    <style>
+        .connection:after {
+            content: "";
+            display: block;
+            width: 1px;
+            background-color: #d9d9d9;
+            height: 100%;
+            position: absolute;
+            left: 5px;
+            z-index: 3;
+            top: 7px;
+        }
+    </style>
+@endsection
